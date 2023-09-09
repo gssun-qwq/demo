39c33807908277e92ab6f0c772aa2c117660a0ef
ShuiYijiao <gssunyongjian@outlook.com>
2023-09-09 13:06:29 +0800

    Initial commit

---
+++ src/test/java/cn/edu/tust/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.tust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
